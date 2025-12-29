af8b9cf3e2cf57ed938c2d302b52c6f69b27220f
AliKanjo <kanjoali38@gmail.com>
2025-12-29 19:09:59 +0300

    Refactor RangePrinter to combine odd and even printing

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -6,23 +6,14 @@ public class RangePrinter {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-for (int i = a + 1; i < b; i++) {
-    if (i % 2 != 0) {
-        System.out.println(i);
-    }
-}
-    for (int i = a + 1; i < b; i++) {
-    if (i % 2 == 0) {
-        System.out.println("Even: " + i);
-    } else {
-        System.out.println("Odd: " + i);
-    }
-}
 
-}
-
-
-
+        for (int i = a + 1; i < b; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even: " + i);
+            } else {
+                System.out.println("Odd: " + i);
+            }
+        }
     }
 }
 
